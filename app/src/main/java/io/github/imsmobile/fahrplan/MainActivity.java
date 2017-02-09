0865ed584d7e527ac84a51d4c7023be6140566e9
Sandro Zbinden <sandrozbinden@gmail.com>
2017-02-09 18:27:06 +0100

    Add basic activity with from and to form that loads other empty activity when search button is pressed.

---
+++ app/src/main/java/io/github/imsmobile/fahrplan/MainActivity.java
@@ -0,0 +1,31 @@
+package io.github.imsmobile.fahrplan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        registerSearchButton();
+    }
+
+    private void registerSearchButton() {
+        Button buttonOne = (Button) findViewById(R.id.btn_search);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                search();
+            }
+        });
+    }
+
+    private void search() {
+        Intent intent = new Intent(this, SearchResultActivity.class);
+        startActivity(intent);
+    }
+}
