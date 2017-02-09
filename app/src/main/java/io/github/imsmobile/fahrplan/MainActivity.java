9679491112793195faf44df427baab03baa1177b
Sandro Zbinden <sandrozbinden@gmail.com>
2017-02-09 18:40:00 +0100

    Add messages (from, to) to intent and display on search result activity

--- app/src/main/java/io/github/imsmobile/fahrplan/MainActivity.java
+++ app/src/main/java/io/github/imsmobile/fahrplan/MainActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String FROM_MESSAGE = "io.github.imsmoble.fahrplan.from";
+    public final static String TO_MESSAGE = "io.github.imsmoble.fahrplan.to";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void search() {
         Intent intent = new Intent(this, SearchResultActivity.class);
+        EditText fromText = (EditText) findViewById(R.id.input_from);
+        String from = fromText.getText().toString();
+
+        EditText toText = (EditText) findViewById(R.id.input_to);
+        String to = toText.getText().toString();
+        intent.putExtra(FROM_MESSAGE, from);
+        intent.putExtra(TO_MESSAGE, to);
         startActivity(intent);
     }
 }
--- app/src/main/java/io/github/imsmobile/fahrplan/SearchResultActivity.java
+++ app/src/main/java/io/github/imsmobile/fahrplan/SearchResultActivity.java
@@ -1,7 +1,10 @@
 package io.github.imsmobile.fahrplan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SearchResultActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class SearchResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+        Intent intent = getIntent();
+        String from = intent.getStringExtra(MainActivity.FROM_MESSAGE);
+        String to = intent.getStringExtra(MainActivity.TO_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText("From: " + from + " To:" + to);
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_search_result);
+        layout.addView(textView);
     }
 }
