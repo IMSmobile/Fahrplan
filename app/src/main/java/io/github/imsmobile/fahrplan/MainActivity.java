6438ed254e4a338d4a419dbf9f541815e33837d6
Sandro Zbinden <sandrozbinden@gmail.com>
2017-02-10 11:56:45 +0100

    Add Toolbar to the activities. With possiblity to return to the previous activity in search result activity. Create content layout that will be used for content

--- app/src/main/java/io/github/imsmobile/fahrplan/MainActivity.java
+++ app/src/main/java/io/github/imsmobile/fahrplan/MainActivity.java
@@ -3,6 +3,7 @@ package io.github.imsmobile.fahrplan;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         registerSearchButton();
     }
 
--- app/src/main/java/io/github/imsmobile/fahrplan/SearchResultActivity.java
+++ app/src/main/java/io/github/imsmobile/fahrplan/SearchResultActivity.java
@@ -3,7 +3,9 @@ package io.github.imsmobile.fahrplan;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -19,11 +21,17 @@ public class SearchResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+        Toolbar myChildToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myChildToolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+
         Intent intent = getIntent();
         String from = intent.getStringExtra(MainActivity.FROM_MESSAGE);
         String to = intent.getStringExtra(MainActivity.TO_MESSAGE);
         textView = new TextView(this);
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_search_result);
+        ViewGroup layout = (ViewGroup) findViewById(R.id.content);
         layout.addView(textView);
         startSearch(from, to);
     }
