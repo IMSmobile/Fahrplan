6809e121ef9adf830a6079664177b46b18e27127
Sandro Zbinden <sandrozbinden@gmail.com>
2017-02-10 14:39:15 +0100

    Add german translation for progress (waiting) dialog

--- app/src/main/java/io/github/imsmobile/fahrplan/ui/ProgressDialogUI.java
+++ app/src/main/java/io/github/imsmobile/fahrplan/ui/ProgressDialogUI.java
@@ -3,6 +3,8 @@ package io.github.imsmobile.fahrplan.ui;
 import android.app.ProgressDialog;
 import android.support.v4.app.FragmentActivity;
 
+import io.github.imsmobile.fahrplan.R;
+
 /**
  * Created by sandro on 09.02.2017.
  */
@@ -11,7 +13,7 @@ public class ProgressDialogUI extends ProgressDialog {
     public ProgressDialogUI(FragmentActivity activity) {
         super(activity);
         setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        setMessage("Loading. Please wait...");
+        setMessage(activity.getResources().getString(R.string.waiting_text));
         setIndeterminate(true);
         setCanceledOnTouchOutside(false);
     }
