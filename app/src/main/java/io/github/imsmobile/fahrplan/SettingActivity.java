af24e0884a58e6332e37132555eb84753db49868
Sandro Zbinden <sandrozbinden@gmail.com>
2017-02-19 00:06:15 +0100

    Add possiblity to persist checkboxe state for selection of transportation. Also set default to true

--- app/src/main/java/io/github/imsmobile/fahrplan/SettingActivity.java
+++ app/src/main/java/io/github/imsmobile/fahrplan/SettingActivity.java
@@ -2,10 +2,14 @@ package io.github.imsmobile.fahrplan;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.widget.AutoCompleteTextView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import io.github.imsmobile.fahrplan.adapter.StationAdapter;
 import io.github.imsmobile.fahrplan.listener.TextWatcherAdapter;
@@ -25,22 +29,38 @@ public class SettingActivity extends AppCompatActivity {
         inputTakeMeHome.addTextChangedListener(new TextWatcherAdapter() {
             @Override
             public void afterTextChanged(Editable e) {
-                saveSettings(e.toString());
+                saveSettings(getString(R.string.setting_key_take_me_home), e.toString());
             }
         });
-        inputTakeMeHome.setText(getSettings(), false);
+        inputTakeMeHome.setText(getSettings(getString(R.string.setting_key_take_me_home), ""), false);
+        initTransportationSelection(R.id.cb_train, R.string.cb_train);
+        initTransportationSelection(R.id.cb_tram, R.string.cb_tram);
+        initTransportationSelection(R.id.cb_bus, R.string.cb_bus);
+        initTransportationSelection(R.id.cb_boat, R.string.cb_boat);
     }
 
-    private void saveSettings(String home) {
+    private void initTransportationSelection(@IdRes int checkboxId , @StringRes int keyResId) {
+        CheckBox transportationCheckbox = (CheckBox) findViewById(checkboxId);
+        final String key = getString(keyResId);
+        transportationCheckbox.setChecked(Boolean.valueOf(getSettings(key, String.valueOf(Boolean.TRUE))));
+        transportationCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                saveSettings(key, String.valueOf(isChecked));
+            }
+        });
+    }
+
+    private void saveSettings(String key, String value) {
         SharedPreferences preferences = this.getSharedPreferences(getString(R.string.setting_name), MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(getString(R.string.setting_key_take_me_home), home);
+        editor.putString(key, value);
         editor.apply();
     }
 
-    private String getSettings() {
+    private String getSettings(String key, String defaultValue) {
         SharedPreferences preferences = this.getSharedPreferences(getString(R.string.setting_name), MODE_PRIVATE);
-        return preferences.getString(getString(R.string.setting_key_take_me_home), "");
+        return preferences.getString(key, defaultValue);
     }
 }
 
