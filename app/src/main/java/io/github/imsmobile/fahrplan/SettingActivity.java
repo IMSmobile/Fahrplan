c78eec0ca433e2ec377bd1e2eef555a04e31ee77
Michael Leu <mleu@users.noreply.github.com>
2017-02-18 16:02:10 +0100

    add Take Me Home address to settings
    plus fix some lint warnings with a dictionary

--- app/src/main/java/io/github/imsmobile/fahrplan/SettingActivity.java
+++ app/src/main/java/io/github/imsmobile/fahrplan/SettingActivity.java
@@ -1,8 +1,15 @@
 package io.github.imsmobile.fahrplan;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.widget.AutoCompleteTextView;
+
+import io.github.imsmobile.fahrplan.adapter.StationAdapter;
+import io.github.imsmobile.fahrplan.listener.TextWatcherAdapter;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -13,6 +20,28 @@ public class SettingActivity extends AppCompatActivity {
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(getResources().getString(R.string.settings_title));
+
+        AutoCompleteTextView inputTakeMeHome = (AutoCompleteTextView) findViewById(R.id.input_take_me_home);
+        inputTakeMeHome.setAdapter(new StationAdapter(this, R.layout.autocomplete_item));
+        inputTakeMeHome.addTextChangedListener(new TextWatcherAdapter() {
+            @Override
+            public void afterTextChanged(Editable e) {
+                saveSettings(e.toString());
+            }
+        });
+        inputTakeMeHome.setText(getSettings());
+    }
+
+    private void saveSettings(String home) {
+        SharedPreferences preferences = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(getString(R.string.setting_key_take_me_home), home);
+        editor.apply();
+    }
+
+    private String getSettings() {
+        SharedPreferences preferences = this.getPreferences(Context.MODE_PRIVATE);
+        return preferences.getString(getString(R.string.setting_key_take_me_home), "");
     }
 }
 
