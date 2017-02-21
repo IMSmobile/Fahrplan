2f407ce11f471f32f41462af2e9e7b675a2ce477
Michael Leu <mleu@users.noreply.github.com>
2017-02-22 00:30:39 +0100

    add basic connection sharing ability

--- app/src/main/java/io/github/imsmobile/fahrplan/ConnectionActivity.java
+++ app/src/main/java/io/github/imsmobile/fahrplan/ConnectionActivity.java
@@ -3,8 +3,13 @@ package io.github.imsmobile.fahrplan;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -12,6 +17,9 @@ import com.google.gson.Gson;
 import ch.schoeb.opendatatransport.model.Connection;
 
 public class ConnectionActivity extends AppCompatActivity {
+
+    private Connection connection;
+    private ShareActionProvider shareAction;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,9 +32,29 @@ public class ConnectionActivity extends AppCompatActivity {
         Gson gson = new Gson();
         Intent intent = getIntent();
 
-        Connection connection = gson.fromJson(intent.getStringExtra(SearchResultActivity.CONNECTION), Connection.class);
+        connection = gson.fromJson(intent.getStringExtra(SearchResultActivity.CONNECTION), Connection.class);
 
         TextView text = (TextView) findViewById(R.id.connection);
         text.setText(connection.toString());
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.share, menu);
+
+        MenuItem item = menu.findItem(R.id.action_share);
+        shareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        Intent send = new Intent();
+        send.setAction(Intent.ACTION_SEND);
+        send.putExtra(Intent.EXTRA_TEXT, connection.toString());
+        send.setType("text/plain");
+        if(shareAction != null) {
+            shareAction.setShareIntent(send);
+        }
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
