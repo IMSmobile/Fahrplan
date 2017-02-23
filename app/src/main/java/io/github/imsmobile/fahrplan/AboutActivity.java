ab5cbc36771a305c53602cff26c7350e6d865576
Niklaus Tschirky <ntschirk@hsr.ch>
2017-02-23 21:44:10 +0100

    Redesigned Aboutscreen Reduced warnings Close #57 (About Screen Redesign)

--- app/src/main/java/io/github/imsmobile/fahrplan/AboutActivity.java
+++ app/src/main/java/io/github/imsmobile/fahrplan/AboutActivity.java
@@ -3,6 +3,7 @@ package io.github.imsmobile.fahrplan;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
     @Override
@@ -12,5 +13,7 @@ public class AboutActivity extends AppCompatActivity {
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(getResources().getString(R.string.label_about));
+        String versionName = "Version " + BuildConfig.VERSION_NAME;
+        ((TextView)findViewById(R.id.label_version)).setText( versionName);
     }
 }
